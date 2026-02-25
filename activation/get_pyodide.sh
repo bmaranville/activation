bd3fb8db1a3474f30acccf25726a5f90fe826a0c
bbm <brian.maranville@nist.gov>
2026-02-25 14:20:05 -0500

    add helper file that contains full path to periodictable local wheel

--- activation/get_pyodide.sh
+++ activation/get_pyodide.sh
@@ -1,5 +1,8 @@
 # !/bin/bash
 
+# Run in the activation directory (where this script lives)
+cd "$(dirname "$0")"
+
 # Default version of pyodide to download if not set via environment variable
 PYODIDE_VERSION=${PYODIDE_VERSION:-0.29.3}
 
@@ -20,3 +23,10 @@ tar -xjf pyodide_full.tar.bz2 -C ./pyodide --strip-components=1 \
 
 # Download the latest periodictable wheel from PyPI
 pip download periodictable --no-deps --only-binary :all: -d ./pyodide/
+
+# Write out the full wheel file name to a text file for use in the workflow
+ls pyodide/periodictable-*.whl > periodictable_wheel_name.txt
+
+# Cleanup
+rm -f pyodide.tar.bz2
+rm -f pyodide_full.tar.bz2
