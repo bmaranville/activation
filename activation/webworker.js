a1fe2e1691b7b0d1e4872d71b7d576617a6e8286
bbm <brian.maranville@nist.gov>
2026-02-27 12:43:04 -0500

    nact.py api_call takes a plain dict (JSON) now, no need to fake CGI field storage

--- activation/webworker.js
+++ activation/webworker.js
@@ -25,18 +25,7 @@ async function loadPyodideAndPackages() {
         f.write(await response.bytes())
     import nact
     import json
-    import re
 
-    class FakeFieldStorage(dict):
-        def getfirst(self, name, default=None):
-            rval = self.get(name, default)
-            return rval if rval != "" else default
-        def getlist(self, name, default=None):
-            name = re.sub("\\[\\]$", "", name)
-            rval = self.get(name, default)
-            if isinstance(rval, str) or isinstance(rval, bytes):
-                return [rval]
-            return list(rval)
   `)
 }
 let pyodideReadyPromise = loadPyodideAndPackages();
@@ -45,16 +34,13 @@ pyodideReadyPromise.then(() => self.postMessage({worker_ready: true}));
 self.onmessage = async (event) => {
   // make sure loading is done
   await pyodideReadyPromise;
-  // Don't bother yet with this line, suppose our API is built in such a way:
-  const { request } = event.data;
-  const json_data = JSON.stringify(event.data.data);
   // Now is the easy part, the one that is similar to working in the main thread:
   try {
+    const json_data = JSON.stringify(event.data.data);
     let python = `
       request = json.loads('${json_data}')
-      form = FakeFieldStorage(request)
       try:
-          response = nact.cgi_call(form)
+          response = nact.api_call(request)
       except Exception:
           response = {
             'success': False,
