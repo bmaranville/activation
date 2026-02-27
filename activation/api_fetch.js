63ed18ccdd78ab1734e83f9fa23a7bec2de41587
bbm <brian.maranville@nist.gov>
2026-02-27 12:41:59 -0500

    just pass JSON

--- activation/api_fetch.js
+++ activation/api_fetch.js
@@ -2,23 +2,19 @@ class FetchAPI {
     response_callback = null;
     url = null;
 
-    initialize (response_callback, ready_callback=() => {}, url="/cgi-bin/nact.py") {
+    initialize (response_callback, ready_callback=() => {}, url="/api/calculate") {
         this.response_callback = response_callback;
         this.url = url;
         ready_callback();
     }
 
     submit(data) {
-        const { rest, ...form_data } = data;
-        const request_body = new URLSearchParams(form_data);
-        // array handling in old URLSearchParams is a bit tricky, so we do it manually:
-        rest.forEach((value) => {
-            request_body.append('rest[]', value);
-        });
-
         fetch(this.url, {
             method: "POST",
-            body: request_body
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json'
+            }
         })
         .then(response => response.json())
         .then(json => this.response_callback(json))
